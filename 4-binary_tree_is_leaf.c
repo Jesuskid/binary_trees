e0fc9c0b480b9498275b08dd610b699537df0dcb
Rawllings <frawllings@example.com>
2023-03-01 06:17:42 -0800

    fourth

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,11 +10,5 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if(!node)
-        return (0);
-        
-    if(node->left != NULL || node->right != NULL)
-        return (1);
-    
-    return (0)
+    return ((!node || node->left || node->right) ? 0 : 1);
 }
